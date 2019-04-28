f190513407579c8cff62275921c8efa3d2e2d00f
Adriel <adriel.idf@gmail.com>
2019-04-28 11:29:59 -0300

    character animation and movement fixes with tech artifice

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -18,45 +18,61 @@ public class Character : MonoBehaviour
 
     #endregion
     public GameObject playerSprite;
-
+    public Animator anim;
+    private bool facingRight = true;
     void Start()
     {
-        
+
     }
-    
+
     void Update()
     {
 
     }
-   
+
     private void FixedUpdate()
     {
         #region Movement & Rotation
         float speedAndTime = Speed * Time.deltaTime;
         float y = Input.GetAxisRaw("Vertical");
         float x = Input.GetAxisRaw("Horizontal");
-        transform.Translate(x * speedAndTime, y * speedAndTime, 0f);
-        if(!(x == 0 && y == 0))//quando possuir animações setar as triggers dentro do calculate rotation em vez de rotacionar o sprite daí
-            playerSprite.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, CalculateRotation(x, y)));//Quaternion.Euler é para converter de graus para o tal do quaternion que ninguem sabe como funciona direito
+        anim.SetBool("running", x != 0 || y != 0);
+        transform.Translate(x < 0 ? (x * speedAndTime) * -1 : x * speedAndTime, y * speedAndTime, 0f);
+        //  if(!(x == 0 && y == 0))//quando possuir animações setar as triggers dentro do calculate rotation em vez de rotacionar o sprite daí
+        //      playerSprite.transform.rotation = Quaternion.Euler(new Vector3(0f, 0f, CalculateRotation(x, y)));//Quaternion.Euler é para converter de graus para o tal do quaternion que ninguem sabe como funciona direito
+        if (x < 0)
+        {
+            facingRight = false;
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+        }
+        else
+        {
+            facingRight = true;
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+        }
+
         #endregion
 
         #region Attack
-
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            anim.SetTrigger("attack");
+        }
 
         #endregion
     }
     private float CalculateRotation(float x, float y)
     {
-        if (Mathf.Abs(x) > Mathf.Abs(y))//Verifica qual possui o valor mais distante de zero dando preferencia para o eixo vertical(melhor nos consoles, no pc é -1,0 ou 1 então nao muda muito)
-            if (x < 0)
-                return 90f;
-            else
-                return 270f;
+        //if (Mathf.Abs(x) > Mathf.Abs(y))//Verifica qual possui o valor mais distante de zero dando preferencia para o eixo vertical(melhor nos consoles, no pc é -1,0 ou 1 então nao muda muito)
+        if (x < 0)
+            return 90f;//quase tudo comentado porque a rotação vai ser só para esquerda e direita
         else
-            if (y < 0)
-            return 180f;
-        else
-            return 0f;
+            return 270f;
+        //else
+        //    if (y < 0)
+        //    return 180f;
+        //else
+        //    return 0f;
 
     }
 }
